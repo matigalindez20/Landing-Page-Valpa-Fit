62e9558d22d08ffcbffd0d10402699cea332d76a
Matias Galindez <matiasgalindez20@gmail.com>
2023-04-18 01:17:45 -0300

    se edita formulario y estilos

---
+++ script.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector("#li-nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () =>{
+    head.classList.add("visible");
+})
